2a099f30bce225e8cc555c321a67b9847b3e04d6
Jorge Aguiar Junior <08220149@aluno.osorio.ifrs.edu.br>
2023-07-28 13:53:33 -0300

    Retirado o erro na diferenca

--- src/main/java/com/calculadoraci/Calculadora.java
+++ src/main/java/com/calculadoraci/Calculadora.java
@@ -35,7 +35,7 @@ public class Calculadora {
     }
 
     public double getDiferenca() {
-        return (getValorA() - getValorB() + 9999);
+        return (getValorA() - getValorB());
     }
 
     public double getProduto() {
